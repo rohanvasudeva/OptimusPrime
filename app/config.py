ed0511250b494a599b62af47ca826e89df6632ec
rohanvasudeva <vasudeva7210@gmail.com>
2026-09-07 15:18:19 +0530

    small changes

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -6,8 +6,10 @@ class Settings(BaseSettings):
     GROQ_API_KEY: str
     GROQ_MODEL: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
 
 settings = Settings()
